2a887177f792428e42770c09907e50c2825073de
csenet <admin@k1h.dev>
2025-11-15 06:11:57 +0900

    update

--- swarm-cd/deploy.sh
+++ swarm-cd/deploy.sh
@@ -5,10 +5,12 @@ set -e
 # Docker Configが既に存在する場合は削除
 docker config rm swarm-cd-repos 2>/dev/null || true
 docker config rm swarm-cd-stacks 2>/dev/null || true
+docker config rm swarm-cd-config 2>/dev/null || true
 
 # Docker Configを作成
 docker config create swarm-cd-repos repos.yaml
 docker config create swarm-cd-stacks stacks.yaml
+docker config create swarm-cd-config config.yaml
 
 # スタックをデプロイ
 docker stack deploy --compose-file docker-compose.yaml swarm-cd
